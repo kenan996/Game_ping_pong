801e34f14bbd95a754b4d14259cb7e3678433478
Kenan Behbudov <kanannbe@code.edu.az>
2018-12-04 15:44:28 +0400

    game

---
+++ Ping_Pong_Game/Ping_Pong_Game/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace Ping_Pong_Game
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.playground = new System.Windows.Forms.Panel();
+            this.racket = new System.Windows.Forms.PictureBox();
+            this.ball = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.playground.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.racket)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ball)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // playground
+            // 
+            this.playground.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.playground.Controls.Add(this.ball);
+            this.playground.Controls.Add(this.racket);
+            this.playground.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.playground.Location = new System.Drawing.Point(0, 0);
+            this.playground.Name = "playground";
+            this.playground.Size = new System.Drawing.Size(675, 399);
+            this.playground.TabIndex = 0;
+            // 
+            // racket
+            // 
+            this.racket.BackColor = System.Drawing.Color.Maroon;
+            this.racket.Location = new System.Drawing.Point(222, 368);
+            this.racket.Name = "racket";
+            this.racket.Size = new System.Drawing.Size(178, 16);
+            this.racket.TabIndex = 0;
+            this.racket.TabStop = false;
+            // 
+            // ball
+            // 
+            this.ball.BackColor = System.Drawing.SystemColors.Highlight;
+            this.ball.Location = new System.Drawing.Point(290, 158);
+            this.ball.Name = "ball";
+            this.ball.Size = new System.Drawing.Size(28, 29);
+            this.ball.TabIndex = 1;
+            this.ball.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(675, 399);
+            this.Controls.Add(this.playground);
+            this.Name = "Form1";
+            this.Text = "`";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.playground.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.racket)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ball)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel playground;
+        private System.Windows.Forms.PictureBox ball;
+        private System.Windows.Forms.PictureBox racket;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Ping_Pong_Game/Ping_Pong_Game/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ping_Pong_Game
+{
+    public partial class Form1 : Form
+    {
+        private int speed_left = 3;
+        private int speed_top = 3;
+        private int point = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+
+            timer1.Enabled = true;
+            Cursor.Hide();
+
+            this.FormBorderStyle = FormBorderStyle.None;
+            this.TopMost = true;
+            this.Bounds = Screen.PrimaryScreen.Bounds;
+
+            racket.Top = playground.Bottom - (playground.Bottom / 10);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            racket.Left = Cursor.Position.X - (racket.Width / 2);
+            ball.Left += speed_left;
+            ball.Top += speed_top;
+            if (ball.Bottom >= racket.Top && ball.Bottom <= racket.Bottom && ball.Right >= racket.Left && ball.Right <= racket.Right)
+            {
+                speed_top += 2;
+                speed_left += 2;
+                speed_top = -speed_top;
+                point += 1;
+            }
+            if (ball.Left <= playground.Left)
+            {
+                speed_left = -speed_left;
+            }
+           if (ball.Right >= playground.Right)
+            {
+                speed_left = -speed_left;
+            }
+           if (ball.Top <= playground.Top)
+            {
+                speed_top = -speed_left;
+            }
+            if (ball.Bottom >= playground.Bottom)
+            {
+                timer1.Enabled = false;
+            }
+          
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Escape)
+            {
+                this.Close();
+            }
+        }
+    }
+}
